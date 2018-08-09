51c611ef1122835de6c54cb50b1d7097c4e6f2df
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-09 11:11:46 +0530

    Create evenorodd.java

---
+++ evenorodd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int i=123;
+		if(i%2==0)
+		{
+			System.out.println("it is even");
+		}
+		else
+		 System.out.println("it is odd");
+	}
+}
