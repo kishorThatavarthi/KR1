5fb5d9d974621602207c0b634fbdef0b9471e9c5
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-09 12:20:08 +0530

    Create Leap year or not.java

---
+++ Leap year or not.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int year=2016;
+		if(year%4==0)
+		  System.out.println("yes");
+		else
+		  System.out.println("No");
+	}
+}
