cefb5eccbb4db14a3223b8bf8bfe8a28ec824586
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-09 11:55:13 +0530

    Create Alphabet or Not.java

---
+++ Alphabet or Not.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		char ch='a';
+		if(Character.isLetter(ch))
+		  System.out.println("Alphabet");
+		else
+		  System.out.println("No");
+	}
+}
