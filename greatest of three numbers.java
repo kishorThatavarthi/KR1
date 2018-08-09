18cf7239840a343053c99406d75698e686ad7b47
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-09 12:00:37 +0530

    Create greatest of three numbers.java

---
+++ greatest of three numbers.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		 int a=-1,b=-2,c=-3;
+		 if(a>b && a>c)
+		 {
+		 	System.out.println(a);
+		 }
+		 else if(b>a && b>c)
+		 {
+		 	System.out.println(b);
+		 }
+		 else
+		  System.out.println(c);
+	}
+}
