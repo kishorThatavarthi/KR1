17176be20f9028ba5a0c3d821faa6680903c07bd
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-10 12:11:05 +0530

    Create odd numbers between two intervals.java

---
+++ odd numbers between two intervals.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n1=2,n2=6,i=0;
+		for(i=n1;i<=n2;i++)
+		{
+			if(i%2==1)
+			 System.out.println(i);
+		}
+	}
+}
