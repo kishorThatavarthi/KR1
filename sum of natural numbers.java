36478020be6dcc2c361b7a10b52aca161e7cacc7
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-10 11:47:56 +0530

    Create sum of natural numbers.java

---
+++ sum of natural numbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n=10,count=0;
+		for(int i=0;i<=n;i++)
+		{
+			count=count+i;
+ 
+		}
+		System.out.println(count);
+	}
+}
