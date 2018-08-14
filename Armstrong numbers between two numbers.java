8f09831fefd82fbac08930469eade21d1c29ef8f
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-14 11:33:32 +0530

    Update Armstrong numbers between two numbers.java

--- Armstrong numbers between two numbers.java
+++ Armstrong numbers between two numbers.java
@@ -1,8 +1,12 @@
+import java.util.*;
 public class Armstrong
 {
     public static void main(String[] args) 
      {
-        int n, count = 0, a, b, c, sum = 0,n1=1,n2=100;
+        int n, count = 0, a, b, c, sum = 0,n1,n2;
+        Scanner input=new Scanner(System.in);
+        n1=input.nextInt();
+        n2=input.nextInt();
      
         for(int i = n1; i <= n2; i++)
         {
