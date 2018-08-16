34c86126f4783de46a33f466ed8a9e6928d40d4b
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-16 11:00:01 +0530

    Update factoral of a number.java

--- factoral of a number.java
+++ factoral of a number.java
@@ -1,7 +1,9 @@
+import java.util.*;
 class FactorialExample{  
  public static void main(String args[]){  
   int i,fact=1;  
-  int number=5;//It is the number to calculate factorial    
+  Scanner input=new Scanner(System.in);
+  int number=input.nextInt();;//It is the number to calculate factorial    
   for(i=1;i<=number;i++){    
       fact=fact*i;    
   }    
