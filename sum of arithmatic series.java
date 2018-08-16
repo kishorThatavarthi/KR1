8483a4ca57dfc8d737916d52647f61ac1c69082d
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-16 11:29:00 +0530

    Create sum of arithmatic series.java

---
+++ sum of arithmatic series.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	static float sumOfAP(float a, float d, 
+                                  int n)
+    {
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum = sum + a;
+            a = a + d;
+        }
+        return sum;
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		 Scanner input=new Scanner(System.in);
+		 
+		int n = input.nextInt();;
+        int a = input.Int(), d = input.Int();
+        System.out.println(sumOfAP(a, d, n));
+	}
+}
