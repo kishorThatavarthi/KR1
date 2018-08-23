d9c84b6bb338b12ccbe0aeab32e76da015e7f485
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-23 10:51:47 +0530

    Update MaximumAmong10.java

--- MaximumAmong10.java
+++ MaximumAmong10.java
@@ -3,7 +3,8 @@ class MaximumAmong10
 {
  public static void main(String[] args)
  {
-  int a[10],max=0;
+  int a[]=new int[10];
+  int max;
   Scanner s=new Scanner(System.in);
   for(int i=0;i<a.length;i++)
   {
