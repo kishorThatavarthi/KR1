738df5be4df6f5673d69e71a17d60d2b6eb73ce4
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-23 09:39:34 +0530

    Create Armstrong Number or not.java

---
+++ Armstrong Number or not.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+class ArmstrongExample{  
+  public static void main(String[] args)  {  
+    int c=0,a,temp;  
+    int n=153;//It is the number to check armstrong  
+    temp=n;  
+    while(n>0)  
+    {  
+    a=n%10;  
+    n=n/10;  
+    c=c+(a*a*a);  
+    }  
+    if(temp==c)  
+    System.out.println("yes");   
+    else  
+        System.out.println("no");   
+   }  
+}  
