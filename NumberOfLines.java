a0165e4490efcc32b953dc1664e8619ae0493d35
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-23 10:08:27 +0530

    Update NumberOfLines.java

--- NumberOfLines.java
+++ NumberOfLines.java
@@ -5,7 +5,7 @@ class NumberOfLines
  {
   Scanner s=new Scanner(System.in);
   String str=s.nextLine();
-  int count=0;
+  int count=1;
   for(int i=0;i<str.length();i++)
   {
    char ch=str.charAt(i);
