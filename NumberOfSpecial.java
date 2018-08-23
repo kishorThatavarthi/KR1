a5b650c8a6d808d10374c497025d2b7cbde315c7
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-23 10:16:38 +0530

    Create NumberOfSpecial.java

---
+++ NumberOfSpecial.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class NumberOfSpecial
+{
+ public static void main(String[] args)
+ {
+  Scanner s=new Scanner(System.in);
+  String str=s.nextLine();
+  int count=0;
+  for(int i=0;i<str.length();i++)
+  {
+   char ch=str.charAt(i);
+   if(Character.isDigit(ch)||Character.isWhitespace(ch)||Character.isLetter(ch))
+   {
+    
+    }
+    else
+    {
+    count++;
+     }
+   }
+   System.out.println(count);
+  }
+ }
