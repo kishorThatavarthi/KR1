0eb56cedd32db2e7297695bb8d9b8458826e67ba
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-23 12:08:56 +0530

    Create StringNTimes.java

---
+++ StringNTimes.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+class StringNTimes
+{
+ public static void main(String[] args)
+ {
+   Scanner s=new Scanner(System.in);
+   String str=s.nextLine();
+   int n=s.nextInt();
+   for(int i=0;i<n;i++)
+   {
+     System.out.println(str);
+     }
+   }
+ }
