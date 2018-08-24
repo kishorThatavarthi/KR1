f1c224e37a83ff1bd7a0747dd3f1685d9db6b482
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-24 10:06:32 +0530

    Create StringConcatenation.java

---
+++ StringConcatenation.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class StringConcatenation
+{
+ public static void main(String args[])
+{
+ Scanner s=new Scanner(System.in);
+ String str=s.next();
+ String str1=s.next();
+ System.out.print(str+str1);
+ }
+}
