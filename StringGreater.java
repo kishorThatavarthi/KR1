c195a7014fef09b6626f3f4f80f3852aa42ea437
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-24 09:54:56 +0530

    Create StringGreater.java

---
+++ StringGreater.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class StringGreater
+{
+ public static void main(String args[])
+{
+ Scanner s=new Scanner(System.in);
+ String str=s.nextLine();
+ String str1=s.nextLine();
+ int i=str.compareTo(str1);
+ int j=str1.compareTo(str);
+ if(i>0 && j<0)
+  System.out.println(str);
+ if(j>0 && i<0)
+  System.out.println(str1);
+ if(i==0)
+  System.out.println(str1);
+ }
+}
