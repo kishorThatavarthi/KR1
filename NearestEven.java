ede861ce1664401e3d319cffac0c211fcf60cd8e
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-24 11:11:26 +0530

    Create NearestEven.java

---
+++ NearestEven.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class NearestEven
+{
+ public static void main(String[] args)
+{
+ Scanner s=new Scanner(System.in);
+ int n=s.nextInt();
+ if(n%2==0)
+   System.out.print(n);
+ else
+   System.out.print(n-1);
+ 
+ }
+}
