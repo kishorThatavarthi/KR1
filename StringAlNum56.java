f6a441963fbf3ea9cba41b5e2ccf7c9619bef4e4
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-27 10:10:17 +0530

    Create StringAlNum56.java

---
+++ StringAlNum56.java
@@ -0,0 +1,26 @@
+ import java.util.*;
+class StringAlNum56
+{
+ public static void main(String[] args)
+{
+ Scanner s=new Scanner(System.in);
+ String str=s.next();
+ boolean a=false,b=false;
+ for(int i=0;i<str.length();i++)
+ {
+   char ch=str.charAt(i);
+   if(Character.isLetter(ch))
+   {
+      a=true;
+    }
+   if(Character.isDigit(ch))
+  {
+     b=true;
+   }
+ }
+ if(a && b)
+   System.out.print("yes");
+ else
+   System.out.print("no");
+ }
+}
