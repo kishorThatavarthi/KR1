7aa67c5846e93559203a5994089838c0463b2020
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-27 09:56:51 +0530

    Create NumberInGiven59.java

---
+++ NumberInGiven59.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class NumbersInGiven59
+{
+ public static void main(String[] args)
+{
+ Scanner s=new Scanner(System.in);
+ int n=s.nextInt();
+ int count=0;
+ while(n>0)
+{
+ n=n/10;
+ count++;
+ }
+ System.out.print(count);
+ 
+ }
+}
