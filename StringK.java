1276b023e4e1da949068a13b31ebe6e6cdf18468
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-28 09:49:08 +0530

    Create StringK.java

---
+++ StringK.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class StringK
+{
+ public static void main(String[] args)
+ {
+  Scanner s=new Scanner(System.in);
+  String str=s.next();
+  int a=s.nextInt();
+  for(int i=0;i<a;i++)
+  {
+   System.out.print(str.charAt(i));
+   }
+ }
+}
