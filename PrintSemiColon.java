ecc02f8d821e0d3bd4231d39139ee6dbdae1d4bb
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-28 10:20:18 +0530

    Create PrintSemiColon.java

---
+++ PrintSemiColon.java
@@ -0,0 +1,14 @@
+import java.util.*; 
+class PrintSemiColon {
+    
+public static void main(String[] args) {
+  Scanner s=new Scanner(System.in);
+  String str=s.nextLine();
+    
+    if(System.out.printf(str)!=null){
+        
+    }
+    
+    
+}
+}
