231ef18bdf5d94e2911909ebe8747b444c79f5d7
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-31 15:40:05 +0530

    Create LandR.java

---
+++ LandR.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class LandR
+{
+ public static void main(String[] args)
+ {
+  Scanner s=new Scanner(System.in);
+  int a=s.nextInt();
+  int b=s.nextInt();
+  int c=s.nextInt();
+  boolean b=false;
+  for(int i=b;i<c;i++)
+  {
+   if(i==a)
+    {
+     b=true;
+     break;
+     }
+   }
+  if(b)
+   System.out.print("yes");
+  else
+    System.out.print("no");
+   }
+ }
