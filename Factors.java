3627aaa6b7c808bcc6523c985314a831e36c025d
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-08-31 16:13:09 +0530

    Update Factors.java

--- Factors.java
+++ Factors.java
@@ -12,7 +12,7 @@ class Factors
 		// your code goes here
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
-		for(int i=1;i<n;i++)
+		for(int i=1;i<=n;i++)
 		{
 			if(n%i==0)
 			 System.out.print(i+" ");
