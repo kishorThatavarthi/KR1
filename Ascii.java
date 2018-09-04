57434488934e5e3d0a530937e7fc9ac832147a84
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-09-04 12:24:13 +0530

    Create Ascii.java

---
+++ Ascii.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Ascii
+{
+ public static void main(String[] args)
+ {
+  Scanner s=new Scanner(System.in);
+  char ch=s.next().charAt(0);
+  System.out.print((int)ch);
+   }
+  }
