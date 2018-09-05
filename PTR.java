f77a01b1729a9145d53c389e5c0aaf5b4bb767e5
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-09-05 11:37:07 +0530

    Create PTR.java

---
+++ PTR.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class PTR
+{
+ public static void main(String[] args)
+ {
+  Scanner s=new Scanner(System.in);
+  int a=s.nextInt();
+  int d=s.nextInt();
+  int n=s.nextInt();
+  System.out.print((int)(a*b*c)/100);
+  }
+ }
