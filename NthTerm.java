34b7a35f1f58b50da56aed6358996672bb750787
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-09-05 11:34:13 +0530

    Create NthTerm.java

---
+++ NthTerm.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class NthTerm
+{
+ public static void main(String[] args)
+ {
+  Scanner s=new Scanner(System.in);
+  int a=s.nextInt();
+  int d=s.nextInt();
+  int n=s.nextInt();
+  System.out.print(a+(2n-1)d);
+  }
+ }
+  
