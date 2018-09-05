ca7b01b162512f210249ef6c43e2e05f234d685d
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-09-05 14:20:30 +0530

    Create K.java

---
+++ K.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class K
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=sc.nextInt();
+		System.out.println(a[k-1]);
+	}
+}
