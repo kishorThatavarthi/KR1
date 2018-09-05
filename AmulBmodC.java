2e696941e2bec94c7c6d914107893a445aed53f5
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-09-05 11:22:49 +0530

    Create AmulBmodC.java

---
+++ AmulBmodC.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class AmulBmodC
+{
+ public static void main(Sting[] args)
+ {
+  Scanner s=new Scanner(System.in);
+  int A=s.nextInt();
+  int B=s.nextInt();
+  int C=s.nextInt();
+  System.out.print((int)(A*B)%C);
+  }
+ }
