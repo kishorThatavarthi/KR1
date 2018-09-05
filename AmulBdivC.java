ac39dd011fd792796ed8e6a1677b0dae29e5cfd1
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-09-05 16:15:19 +0530

    Create AmulBdivC.java

---
+++ AmulBdivC.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class AmulBdivC
+{
+ public static void main(String[] args)
+ {
+  Scanner s=new Scanner(System.in);
+  int A=s.nextInt();
+  int B=s.nextInt();
+  int C=s.nextInt();
+  System.out.print((int)(A*B)/C);
+  }
+ }
