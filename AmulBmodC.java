4793d453a3f5994c6ac33e100466bdbede86ed81
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-09-05 11:24:38 +0530

    Update AmulBmodC.java

--- AmulBmodC.java
+++ AmulBmodC.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class AmulBmodC
 {
- public static void main(Sting[] args)
+ public static void main(String[] args)
  {
   Scanner s=new Scanner(System.in);
   int A=s.nextInt();
