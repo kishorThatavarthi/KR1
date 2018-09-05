80b3a5afd30a5db6c20681a6b1c92c21861dfd2e
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-09-05 16:08:50 +0530

    Create Concat.java

---
+++ Concat.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Concat
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		String str1=s.next();
+		System.out.print(str+str1);
+	}
+}
