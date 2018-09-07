29e607b0e81aaa54674d0ef0c63134359e9dbc02
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-09-07 14:30:00 +0530

    Create Player11.java

---
+++ Player11.java
@@ -0,0 +1,28 @@
+class Player11
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		switch(str)
+		{
+			case "Sunday" : System.out.print("yes");
+			                break;
+			case "Monday" : System.out.print("no");
+			                break;
+			case "Tuesday" : System.out.print("no");
+			                break;
+			 case "Wednesday" : System.out.print("no");
+			                break;
+			 case "Thursday" : System.out.print("no");
+			                break;
+		     case "Friday" : System.out.print("no");
+			                break;
+			 case "Satday" : System.out.print("no");
+			                break;
+			                
+			                
+		}
+	}
+}
