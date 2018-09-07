9c397745dc66cbf61a3172d41f18e3fd25420ceb
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-09-07 14:13:34 +0530

    Create Player4.java

---
+++ Player4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Player4
+{
+ public static void main(String[] args)
+ {
+  Scanner s=new Scanner(System.in);
+  String str=s.next();
+  System.out.print(str+".");
+  }
+ }
+  
