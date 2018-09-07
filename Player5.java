2421645286e8cf57e75d8cd4a8bdf9c1be806ff2
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-09-07 14:15:26 +0530

    Create Player5.java

---
+++ Player5.java
@@ -0,0 +1,51 @@
+import java.util.*;
+class Player5
+{
+ public static void main(String[] args)
+ {
+  Scanner s=new Scanner(System.in);
+  String str=s.next();
+  switch(str)
+  {
+    case "I" : System.out.print(1);
+               break;
+   case "II" : System.out.print(2);
+               break;
+    case "III" : System.out.print(3);
+               break;
+   case "IV" : System.out.print(4);
+               break;
+   case "V" : System.out.print(5);
+               break;
+   case "VI" : System.out.print(6);
+               break;
+   case "VII" : System.out.print(7);
+               break;
+   case "VIII" : System.out.print(8);
+               break;
+    case "IX" : System.out.print(9);
+               break;
+    case "X" : System.out.print(10);
+               break;
+    case "XI" : System.out.print(11);
+               break;
+    case "XII" : System.out.print(12);
+               break;
+    case "XIV" : System.out.print(14);
+               break;
+    case "XV" : System.out.print(15);
+               break;
+   case "XVI" : System.out.print(16);
+               break;
+   case "XVII" : System.out.print(17);
+               break;
+    case "XVIII" : System.out.print(18);
+               break;
+    case "XIX" : System.out.print(19);
+               break;
+    case "XX" : System.out.print(20);
+               break;
+  }
+ }
+}
+  
