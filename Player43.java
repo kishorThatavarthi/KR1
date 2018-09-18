b629da4f428dd57555b2898062096354955d03a5
kishorThatavarthi <37865716+kishorThatavarthi@users.noreply.github.com>
2018-09-18 14:11:34 +0530

    Create Player43.java

---
+++ Player43.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Player43
+{
+ public static void main(String[] args)
+{
+ Scanner s=new Scanner(System.in);
+ String str=s.next();
+ String str1=s.next();
+ for(int i=0;i<str.length();i++)
+ {
+  if(str.substring(i).equals(str1))
+    b=true;
+  }
+ if(b)
+   System.out.print("yes");
+ else
+   System.out.print("no");
+}
+}
+ 
